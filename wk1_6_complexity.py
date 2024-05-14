c7a3d896cd4499495ab08ed0a037bfcde5e443aa
Ruitong Sun <rs2177@cam.ac.uk>
2024-05-14 14:07:04 +0100

    Corrections and optimise efficiency for algorithms.

--- wk1_6_complexity.py
+++ wk1_6_complexity.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import timeit
 
 from network_def import Network
-from wk1_1_naive_sampling import rm_graph_gen
-from wk1_5_eff_sampling import *
+from wk1_sampling import *
 
 
 def time_compare():
@@ -13,18 +12,15 @@ def time_compare():
 
     t_ary_1 = np.empty(len(n_ary))
     t_ary_2 = np.empty(len(n_ary))
-    t_ary_3 = np.empty(len(n_ary))
     for i, n in enumerate(n_ary):
         p = 10/(n-1)
-        t_ary_1[i] = timeit.timeit(lambda: rm_graph_gen(n, p, True), number=100)
-        t_ary_2[i] = timeit.timeit(lambda: rm_graph_gen2(n, p, True), number=100)
-        t_ary_3[i] = timeit.timeit(lambda: rm_graph_gen3(n, p, True), number=100)
+        t_ary_1[i] = timeit.timeit(lambda: rm_graph_gen(n, p, True), number=10)
+        t_ary_2[i] = timeit.timeit(lambda: rm_graph_gen2(n, p, True), number=10)
     print("time for naive sampling algorithm:")
     print(t_ary_1)
     print(f"time for 2-stage algorithm:")
     print(t_ary_2)
-    print(f"time for inbuilt algorithm:")
-    print(t_ary_3)
+
 
 
     # Perform linear regression in log scale
@@ -32,16 +28,13 @@ def time_compare():
     t_1_fit = np.poly1d(coeff_1) 
     coeff_2= np.polyfit(np.log(n_ary), np.log(t_ary_2), deg=1)
     t_2_fit = np.poly1d(coeff_2) 
-    coeff_3= np.polyfit(np.log(n_ary), np.log(t_ary_3), deg=1)
-    t_3_fit = np.poly1d(coeff_3) 
 
     # plot the results in log scale
     plt.loglog(n_ary, t_ary_1, "b", label="naive sampling")
     plt.loglog(n_ary, np.exp(t_1_fit(np.log(n_ary))), "--b", label=f"naive slope: {coeff_1[0]:.2f}")
     plt.loglog(n_ary, t_ary_2, "g", label="2-stage sampling")
     plt.loglog(n_ary, np.exp(t_2_fit(np.log(n_ary))), "--g", label=f"2-stage slope: {coeff_2[0]:.2f}")
-    plt.loglog(n_ary, t_ary_3, "r", label="inbuilt sampling")
-    plt.loglog(n_ary, np.exp(t_2_fit(np.log(n_ary))), "--r", label=f"inbuilt slope: {coeff_3[0]:.2f}")
+
 
     # plt.plot(n_ary, t_ary_1, label="naive sampling")
     # plt.plot(n_ary, t_ary_2, label="2-stage sampling")
@@ -50,7 +43,7 @@ def time_compare():
     plt.legend()
     plt.show()
 
-    return t_ary_1, t_ary_2, t_ary_3
+    return t_ary_1, t_ary_2
 
 
 
