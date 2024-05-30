871e225cf8f345bfaaf17aa439fc9d25ee118017
Ruitong Sun <rs2177@cam.ac.uk>
2024-05-30 16:38:18 +0100

    wk4 assignments

--- wk3_5_SIR_cik.py
+++ wk3_5_SIR_cik.py
@@ -8,86 +8,96 @@ from network_def import Network
 from wk3_4_SIR_djset import *
 
 
-def SIR_cik(n, edge_ls, single_node = False):
+def SIR_cik(n, edge_ls, avg_n=100): 
     """Simulate the SIR model on the given network represented by disjoint set.
-    Assume single seed node to be node 0."""
+    To return the average cluster size of one random node after going through k edges.
+    avg_n: to average over avg_n number of nodes (with different degrees), assuming each of them as initial seed node, respectively."""
 
     C = DisjointSet(range(n))
     m = edge_ls.shape[0] # Total number of edges
     seed_node = np.random.randint(n) # Randomly assign seed node
     
     # cluster size containing node i in [0,n) after going through k edges in [0,m]
-    ci_k = np.zeros((m,n)) 
+    ci_k = np.zeros((m,avg_n), dtype=np.int32) 
+    all_infection = False
     for k, edge in enumerate(edge_ls):
-        ci_k_ary = np.zeros(n) # store the cluster size of each node after going through k edges
+        if all_infection:
+            print("All nodes are infected!!! Exit loop.")
+            break
+        ci_k_ary = np.zeros(avg_n, dtype=np.int32) # to store the cluster size of each of the first avg_n nodes after going through k edges
         C.merge(edge[0], edge[1])
 
-        for subset in C.subsets():
-            subset_size = len(subset)
-            if subset_size == n: # if the subset is the whole network, then break
-                ci_k[k:] = n*np.ones((m-k,n))
+        for i in range(avg_n):
+            ci_k_ary[i] = C.subset_size(i)
+            if ci_k_ary[i] == n:
+                ci_k[k:] = n*np.ones((m-k,avg_n), dtype=np.int32)
+                all_infection = True
+                print("All nodes are infected.")
                 break
-            ci_k_ary[subset] = subset_size
+
         ci_k[k] = ci_k_ary       
 
-        print(f"edge {k+1}/{m} done: ci_k = {ci_k[k]}")
+        # print(f"edge {k+1}/{m} done: range of ci_k = {min(ci_k[k])}, {max(ci_k[k])}.")
 
     # inital cluster size is 1 for each seed node (i.e. concatenate with a row of 1s)
-    ci_k = np.concatenate((np.ones((1,n)), ci_k), axis=0) 
-    print("shape of ci_k:", ci_k.shape)
+    ci_k = np.concatenate((np.ones((1,avg_n), dtype=np.int32), ci_k), axis=0) 
+    print("shape of ci_k:", ci_k.shape) # shape(m+1,avg_n)
 
-    if single_node: # take the column of the matrix that corresponds to the seed node
-        # shape(m+1,1)
-        return ci_k[:,seed_node] # FIXME: or should we fix the seed node to always the first node???
-    else:
-        # shape(m+1,n)
-        return ci_k
+    return ci_k # shape(m+1,avg_n)
 
 
 def SIR_bino_coeff(edge_ls, lambda_ary):
-    
+    """Compute the binomial coefficient for each lambda value and each number of edges infected."""
     m = edge_ls.shape[0] # Total number of edges
-    k_ary = np.arange(m+1) # number of edges infected
+    k_ary = np.arange(m+1, dtype=np.int32) # number of edges infected
     lam_n = len(lambda_ary) # number of lambda values
-    bino_coeff = np.zeros((lam_n, m+1)) # store the binomial coefficients for each lambda
+    bino_coeff = np.zeros((lam_n, m+1), dtype=np.float32) # store the binomial coefficients for each lambda
     for idx, lambda_ in enumerate(lambda_ary):
         bino_coeff[idx] = binom.pmf(k_ary, m, lambda_) # shape(lam_n, m+1)
     
     # check the shape of the coefficient array is (lam_n, m+1)
     print("shape of bino coeff:", bino_coeff.shape)
     print(bino_coeff)
-    # shape(lam_n, m+1)
-    return bino_coeff
 
-def SIR_ci_lambda(n, edge_ls, lambda_ary, single_node = False):
-    ci_lambda = np.matmul(SIR_bino_coeff(edge_ls, lambda_ary), SIR_cik(n, edge_ls, single_node))
+    return bino_coeff # shape(lam_n, m+1)
 
-    # shape (lam_n, n) 
-    # each row is the cluster sizes of all nodes for the particular lambda corresponding to that row
-    print("shape of ci_lambda:", ci_lambda.shape)
 
-    return ci_lambda
+
+def SIR_ci_lambda(n, edge_ls, lambda_ary, avg_n=100, compute_mean=True):
+    """Compute average final cluster size containing a random seed node (aftering looping over all edges) for each lambda value.
+    by dot product of binomial coefficient and cluster size array (as a function of k)."""
+
+    ci_lambda = np.matmul(SIR_bino_coeff(edge_ls, lambda_ary), SIR_cik(n, edge_ls, avg_n=avg_n))
+
+    
+    # each row is the cluster sizes of first avg_n nodes for the particular lambda corresponding to that row
+    print("shape of all ci_lambda (not averaged):", ci_lambda.shape) # shape (lam_n, n_avg) 
+
+    if compute_mean:
+        # average over all avg_n nodes to get the average cluster size for each lambda (for a random node)
+        ci_lambda = np.mean(ci_lambda, axis=1) # shape (lam_n,)
+
+    return ci_lambda 
 
 
 if __name__ == "__main__":
     n = 10000
     mean = 20
     lambda_ary = np.linspace(0.00, 0.15, 16) 
-    iter_n = 10
+    avg_n = 100
     mu = np.zeros(len(lambda_ary)) # Mean of the number of total infections mu over lambda
     cov = np.zeros(len(lambda_ary)) # coefficient of variation of the number of total infections sigma / mu over lambda
 
     # Generate a new network for each iteration, and calculate the number of total infections for each lambda
-    output = np.empty(iter_n, dtype=object) # Store the number of total infections for each iteration and each lambda
+    # output = np.empty(iter_n, dtype=object) # Store the number of total infections for each iteration and each lambda
     # generate one single network for all iterations
     edge_ls = config_graph_edge_ls(n, deg_dist_poisson(n, mean))
     
     
-    # TODO: Run for one iteration only, take mean and cov over all nodes
-    edge_ls = np.random.permutation(edge_ls) # generate an edge list
-    output= SIR_ci_lambda(n, edge_ls, lambda_ary) # output shape (lam_n, n)
+    # Run for one iteration only, take mean and cov over first 100 nodes
+    output= SIR_ci_lambda(n, edge_ls, lambda_ary, avg_n, False) # output shape (lam_n, avg_n)
     print("output matrix:", output) 
-    # Compute mean and coefficient of variation over all nodes
+    # Compute mean and coefficient of variation over all avg_n nodes
     mu = np.mean(output, axis=1)        # shape (lam_n,)
     cov = np.std(output, axis=1) / mu   # shape (lam_n,)
     
--- wk4_vaccine.py
+++ wk4_vaccine.py
@@ -3,61 +3,94 @@ import matplotlib.pyplot as plt
 
 from wk2_1_config_model import *
 from wk4_xi import *
+from wk3_4_SIR_djset import config_graph_edge_ls
+from wk3_5_SIR_cik import SIR_ci_lambda
 
 # denote vaccination rate as v
 
 
-n = 10000
-mean = 20
-v_ary = [0.0, 0.2, 0.4]
-lam_ary = np.linspace(0, 0.8, 17)
-
-
-
-def vaccine_adjm(adj_m, v):
-    """Vaccinate a fraction v of the nodes in the network by removing those nodes from the network."""
-    n = adj_m.shape[0]
-    v_n = int(n * v)
-    vaccinated_nodes = np.random.choice(n, v_n, replace=False)
+# def vaccine_adjm(adj_m, v):
+#     """Vaccinate a fraction v of the nodes in the network by removing those nodes from the network."""
+#     n = adj_m.shape[0]
+#     v_n = int(n * v)
+#     vaccinated_nodes = np.random.choice(n, v_n, replace=False)
     
-    # create a mask to keep nodes that are not vaccinated
-    mask = np.ones(n, dtype=bool)
-    mask[vaccinated_nodes] = False
+#     # create a mask to keep nodes that are not vaccinated
+#     mask = np.ones(n, dtype=bool)
+#     mask[vaccinated_nodes] = False
 
-    # remove the vaccinated nodes from the adjacency matrix
-    adj_m = adj_m[mask][:, mask]
-    print(adj_m.shape)
+#     # remove the vaccinated nodes from the adjacency matrix
+#     adj_m = adj_m[mask][:, mask]
+#     print(adj_m.shape)
 
-    return adj_m
+#     return adj_m
+
+def vaccine_edgels(edge_ls, v, nodes_to_remove=None):
+    """Vaccinate a fraction v of the nodes in the network by removing those nodes from the network."""
+    # Get the unique nodes from the edge list
+    nodes = np.unique(edge_ls)
+    num_nodes = len(nodes)
+    num_remove = int(v * num_nodes)
+    
+    if nodes_to_remove is None:
+        # Randomly select nodes to remove
+        nodes_to_remove = np.random.choice(nodes, num_remove, replace=False)
+    
+    # Create a mask to keep edges where neither node is removed
+    mask = np.isin(edge_ls, nodes_to_remove)
+    mask = ~np.any(mask, axis=1)
+    
+    # Update the edge list by removing edges connected to the selected nodes
+    updated_edge_ls = edge_ls[mask]
+    
+    print(updated_edge_ls.shape)
+    print(f"Removed {num_remove} nodes ({v*100:.2f}%) from the network.")
+    return updated_edge_ls
+
+def edge_to_adj_ls(edge_ls, n):
+    """Convert edge list to adjacency list."""
+    adj_ls = [set() for _ in range(n)]
+    for edge in edge_ls:
+        if edge[0] != edge[1]:
+            adj_ls[edge[0]].add(edge[1])
+            adj_ls[edge[1]].add(edge[0])
+    return adj_ls
+
+def sample_nb(v, n):
+    """Sample the number of nodes to vaccinate."""
+    return None
+
+if __name__=="__main__":
+        
+    n = 10000
+    mean = 20
+    v_ary = [0.0, 0.2, 0.4]
+    lam_ary = np.linspace(0, 0.3, 30)
+    avg_n = 100
 
 
+    # generate one single network edge list
+    edge_ls = config_graph_edge_ls(n, deg_dist_poisson(n, mean))
 
-# n = 10000
-# mean = 20
-# lambda_ary = np.linspace(0.00, 0.15, 16) 
-# iter_n = 10
-# mu = np.zeros(len(lambda_ary)) # Mean of the number of total infections mu over lambda
-# cov = np.zeros(len(lambda_ary)) # coefficient of variation of the number of total infections sigma / mu over lambda
+    # run simulations for each vaccination rate
+    outputs = np.empty(len(v_ary), dtype=object)
+    for idx, v in enumerate(v_ary):
+        print(f"Processing vaccination rate: {v}")
+        if v == 0:
+            edge_ls_v = edge_ls # no vaccination
+        else:
+            edge_ls = np.random.permutation(edge_ls) # reshuffle the edge list
+            edge_ls_v = vaccine_edgels(edge_ls, v) # remove edges connected to vaccinated nodes
 
-# # Generate a new network for each iteration, and calculate the number of total infections for each lambda
-# output = np.empty(iter_n, dtype=object) # Store the number of total infections for each iteration and each lambda
-# # generate one single network for all iterations
-# edge_ls = config_graph_edge_ls(n, deg_dist_poisson(n, mean))
-
-
-# # TODO: Run for one iteration only, take mean and cov over all nodes
-# edge_ls = np.random.permutation(edge_ls) # generate an edge list
-# output= SIR_ci_lambda(n, edge_ls, lambda_ary) # output shape (lam_n, n)
-# print("output matrix:", output) 
-# # Compute mean and coefficient of variation over all nodes
-# mu = np.mean(output, axis=1)        # shape (lam_n,)
-# cov = np.std(output, axis=1) / mu   # shape (lam_n,)
-
-# print("mu:", mu)
-# print("cov:", cov)
-# plt.figure()
-# plt.errorbar(lambda_ary, mu, yerr=cov, fmt='o')
-# plt.xlabel("Transmission rate")
-# plt.ylabel("Average number of total infections")
-# plt.savefig("wk3_5_SIR_cik.png")
-# plt.show()    
+        output = SIR_ci_lambda(n, edge_ls_v, lam_ary, avg_n, compute_mean=True) # output shape (lam_n,)
+        outputs[idx] = output
+        
+    # outputs shape (v_n, lam_n)
+    # plot the results
+    plt.figure()
+    for idx, v in enumerate(v_ary):
+        plt.plot(lam_ary, outputs[idx], "o-", label=f"Vaccination rate: {v}")
+    plt.xlabel("Transmission rate")
+    plt.ylabel("Average number of total infections")
+    plt.legend()
+    plt.show()
